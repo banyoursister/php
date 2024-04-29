53c013761525f988b91337de1cf046285191d523
banyoursister <142887307+banyoursister@users.noreply.github.com>
2024-04-29 13:32:23 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+header("HTTP/1.1 ".$_GET["code"]." Found");
+header("Location: ".$_GET["url"]);
+header("Content-Type:".base64_decode($_GET["content-type"]));
+header("Cache-Control: max-age=0, must-revalidate, no-cache, no-store, private");
+header("Pragma: no-cache");
+echo base64_decode($_GET["body"]);
